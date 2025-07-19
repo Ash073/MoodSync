1049b5013c33ead51b1ba3d853aefbdb7f9d276e
Sayyed Ashif <sayyedashif73@gmail.com>
2025-07-20 00:17:04 +0530

    Initial Mood Sync frontend and backend

--- backend/routes/moodRoutes.js
+++ backend/routes/moodRoutes.js
@@ -1,5 +1,5 @@
 // backend/routes/moodRoutes.js
-const express = import("express");
+import express from "express";
 const router = express.Router();
 const { saveMood, getMoodHistory } = import("../controllers/moodController.js");
 const protect = import("../middleware/authMiddleware.js");
@@ -7,5 +7,3 @@ const protect = import("../middleware/authMiddleware.js");
 router.post("/", protect, saveMood);
 router.get("/", protect, getMoodHistory);
 export default router;
-
-module.exports = router;
