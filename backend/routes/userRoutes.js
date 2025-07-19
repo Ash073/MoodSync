17659620632e008ff30338f98077ee2014e62175
Sayyed Ashif <sayyedashif73@gmail.com>
2025-07-20 00:13:10 +0530

    Initial Mood Sync frontend and backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,4 +6,4 @@ import { registerUser, loginUser } from "../controllers/userController.js";
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 export default router;
-module.exports = router;
+
